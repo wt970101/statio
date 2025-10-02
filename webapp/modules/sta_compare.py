019578fc176fa0af56c09b32834777382ef55338
Tim <wt970101@gmail.com>
2025-10-03 00:50:00 +0800

    renew web & revise code

--- webapp/modules/sta_compare.py
+++ webapp/modules/sta_compare.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
 def get_data(keyword):
-    service = Service(r"C:\MyProject\webapp\driver\chromedriver.exe")
+    service = Service(r"C:\MyApps\statio\webapp\driver\chromedriver.exe")
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")  # 背景執行
     browser = webdriver.Chrome(service=service, options=options)
@@ -20,9 +20,9 @@ def get_data(keyword):
             # 樂天價格
             for r in items_rakuten:
                 if r['name'] == item['name'] and r['price'] is not None:
-                    prices.append({'site': 'rakuten', 'price': r['price'], 'url': r['url']})
+                    prices.append({'site': '樂天市場購物網', 'price': r['price'], 'url': r['url']})
             # 9*9 價格
-            prices.append({'site': '9x9', 'price': item['price'], 'url': item['url']})
+            prices.append({'site': '九乘九文具專家', 'price': item['price'], 'url': item['url']})
 
             # 至少有一個價格才加入
             if prices:
@@ -92,14 +92,18 @@ def crawl_rakuten(browser, search_table):
         browser.get(url)
         time.sleep(0.2)
         bsoup = BeautifulSoup(browser.page_source, 'html.parser')
+        commodity = bsoup.find('div', {'class': '_6xzpdb0 _6xzpdbpc _6xzpdbfr _8k5k3g2 _6xzpdbtc _16u6wpd3 _6xzpdb4a _6xzpdb51 _6xzpdb5s _6xzpdb6j _6xzpdb0 _6xzpdbb _6xzpdbpc _6xzpdb8 _6xzpdb1r _6xzpdbj _16u6wpd4'})
+        recommend = commodity.find('div', {'class', '_4z4ak1 _6xzpdb8 _6xzpdbc _6xzpdbyv _6xzpdbyx _6xzpdb9j qa-searchResults-storeRecommend-label'})
+        if recommend != None:
+            continue
         pinfo = bsoup.find('div', {'class': '_16u6wpdb'})
         if not pinfo:
             datadict.append({'site': 'rakuten', 'name': keyword, 'price': None, 'url': None})
         else:
-            # 樂天抓價格
+            # 抓價格
             price_tag = pinfo.find('div', {'class': "_1md3074m"})
             price = int(re.sub(r'[^\d]', '', price_tag.get_text())) if price_tag else None
-            # 樂天抓連結
+            # 樂抓連結
             a_tag = pinfo.find('a')
             link = a_tag['href'] if a_tag else None
             datadict.append({'site': 'rakuten', 'name': keyword, 'price': price, 'url': link})
